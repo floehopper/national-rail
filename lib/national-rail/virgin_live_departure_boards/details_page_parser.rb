93127e7adc7adf13c665b62f42da144b01dff1be
James Mead <james@floehopper.org>
2011-02-14 22:03:27 +0000

    I'm hoping it'll be easier to work out whether we've crossed a day boundary by stepping backwards from the current time through the previous calling points.

--- lib/national-rail/virgin_live_departure_boards/details_page_parser.rb
+++ lib/national-rail/virgin_live_departure_boards/details_page_parser.rb
@@ -15,25 +15,25 @@ module NationalRail
           (table/"tbody tr").each do |tr|
             tds = tr/"td"
             next unless tds.length == 3
-            will_call_at << {
+            will_call_at.push({
               :station => cell_text(tds[0]),
               :timetabled_arrival => @time_parser.parse(cell_text(tds[1])),
               :expected_arrival => @time_parser.parse(cell_text(tds[2]))
-            }
+            })
           end
         end
         previous_calling_points = []
         table = @doc/"table[@summary='Previous calling points']"
         if table.any?
-          (table/"tbody tr").each do |tr|
+          (table/"tbody tr").reverse.each do |tr|
             tds = tr/"td"
             next unless tds.length == 4
-            previous_calling_points << {
+            previous_calling_points.unshift({
               :station => cell_text(tds[0]),
               :timetabled_departure => @time_parser.parse(cell_text(tds[1])),
               :expected_departure => @time_parser.parse(cell_text(tds[2])),
               :actual_departure => @time_parser.parse(cell_text(tds[3]))
-            }
+            })
           end
         end
         { :will_call_at => will_call_at, :previous_calling_points => previous_calling_points }
